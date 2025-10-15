ac27afdc26ef2245e3b8cd39a9cb60434f302b1e
soraoo <zxc147895412@gmail.com>
2025-10-15 10:48:56 +0800

    用户验证相关

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from pathlib import Path
+from typing import Optional
 
 import click
 from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Base(DeclarativeBase):
@@ -17,11 +20,22 @@ app.config["SECRET_KEY"] = "dev"
 
 db = SQLAlchemy(app, model_class=Base)
 
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 
-class User(db.Model):
+
+class User(db.Model, UserMixin):
     __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(20))
+    username: Mapped[str] = mapped_column(String(20))
+    password_hash: Mapped[Optional[str]] = mapped_column(String(128))
+
+    def set_password(self, password: str):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password: str):
+        return check_password_hash(self.password_hash, password)
 
 
 class Movie(db.Model):
@@ -62,7 +76,8 @@ def forge():
         {'title': 'The Pork of Music', 'year': '2012'},
     ]
 
-    user = User(name=name)
+    user = User(name=name, username="soraoo")
+    user.set_password("123456")
     db.session.add(user)
 
     for m in movies:
@@ -71,6 +86,33 @@ def forge():
 
     db.session.commit()
     click.echo("生成完毕。")
+
+
+@app.cli.command()
+@click.option("--username", prompt=True, help="登录用户名")
+@click.option("--password", prompt=True, hide_input=True, confirmation_prompt=True, help="登录密码")
+def admin(username: str, password: str):
+    db.create_all()
+
+    user = db.session.execute(select(User)).scalar()
+    if user is not None:
+        click.echo("更新用户...")
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo("创建用户...")
+        user = User(username=username, name="Admin")
+        user.set_password(password)
+        db.session.add(user)
+
+    db.session.commit()
+    click.echo("完成")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = db.session.get(User, int(user_id))
+    return user
 
 
 @app.context_processor
@@ -82,6 +124,8 @@ def inject_user():
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        if not current_user.is_authenticated:
+            return redirect(url_for("index"))
         title = request.form.get("title").strip()
         year = request.form.get("year").strip()
         if not title or not year or len(year) > 4 or len(title) > 60:
@@ -99,6 +143,7 @@ def index():
 
 
 @app.route("/movie/edit/<int:movie_id>", methods=["GET", "POST"])
+@login_required
 def edit(movie_id: int):
     movie = db.get_or_404(Movie, movie_id)
     if request.method == "POST":
@@ -118,12 +163,60 @@ def edit(movie_id: int):
 
 
 @app.route("/movie/delete/<int:movie_id>", methods=["POST"])
+@login_required
 def delete(movie_id: int):
     movie = db.get_or_404(Movie, movie_id)
     db.session.delete(movie)
     db.session.commit()
     flash("删除成功")
     return redirect(url_for("index"))
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username").strip()
+        password = request.form.get("password").strip()
+
+        if not username or not password:
+            flash("无效输入")
+            return redirect(url_for("login"))
+
+        user = db.session.execute(select(User).filter_by(username=username)).scalar()
+        if user is not None and user.validate_password(password):
+            login_user(user)
+            flash("登录成功")
+            return redirect(url_for("index"))
+
+        flash("用户名或密码无效")
+        return redirect(url_for("index"))
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("再见")
+    return redirect(url_for("index"))
+
+
+@app.route("/settings", methods=["GET", "POST"])
+@login_required
+def settings():
+    if request.method == "POST":
+        name = request.form.get("name")
+        if not name or len(name) > 20:
+            flash("无效输入")
+            return redirect(url_for("settings"))
+
+        current_user.name = name
+        db.session.commit()
+        flash("更新成功")
+        return redirect(url_for("index"))
+
+    return render_template("settings.html")
 
 
 @app.errorhandler(404)
